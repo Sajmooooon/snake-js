98a7a3d46e32245490aa4de85267ab844f9e39e2
simon <sajmoooonsk@gmail.com>
2022-08-02 20:06:57 +0200

    feat: add back movement protection

--- js/script.js
+++ js/script.js
@@ -74,20 +74,28 @@ function rectangle(color,x,y,width,height){
 function keyPush(event){
     switch(event.key){
         case 'ArrowUp':
-            velocityX = 0;
-            velocityY = -1;
+            if (velocityY !== 1) {
+                velocityX = 0;
+                velocityY = -1;
+            }
             break;
         case 'ArrowDown':
-            velocityX = 0;
-            velocityY = 1;
+            if (velocityY !== -1){
+                velocityX = 0;
+                velocityY = 1;
+            }
             break;
         case 'ArrowRight':
-            velocityX = 1;
-            velocityY = 0;
+            if (velocityX !== -1) {
+                velocityX = 1;
+                velocityY = 0;
+            }
             break;
         case 'ArrowLeft':
-            velocityX = -1;
-            velocityY = 0;
+            if (velocityX !== 1) {
+                velocityX = -1;
+                velocityY = 0;
+            }
             break;
     }
 }

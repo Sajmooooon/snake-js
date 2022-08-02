c226d577429a7cdb233630f5b590c88af327b472
simon <sajmoooonsk@gmail.com>
2022-08-02 18:06:56 +0200

    feat: add snake loop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
-//snake
+//canvas
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-//canvas
+//snake
 const snakeSize = 50;
 let snakePosX = 0;
 let snakePosY = canvas.height/2 - snakeSize/2
+let snakeSpeed = 5;
 
-context.fillStyle = 'white';
-context.fillRect(0,0, canvas.width, canvas.height);
 
-context.fillStyle = 'black';
-context.fillRect(snakePosX,snakePosY, snakeSize, snakeSize);
+//loop
+function gameLoop(){
+    context.fillStyle = 'white';
+    context.fillRect(0,0, canvas.width, canvas.height);
+
+    snakePosX += snakeSpeed;
+
+    if (snakePosX > canvas.width){
+        snakePosX = 0
+    }
+
+    if (snakePosY > canvas.height){
+        snakePosY = 0
+    }
+
+    context.fillStyle = 'black';
+    context.fillRect(snakePosX,snakePosY, snakeSize, snakeSize);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
